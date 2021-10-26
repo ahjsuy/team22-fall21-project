3395a2560dc396b8e286893103e72bbe40b3b290
hyi96 <yihao0906@gmail.com>
2021-10-26 14:59:05 -0700

    added card image resource

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Recipe from './Components/recipe'
 
-function App() {
-  return (
-   <Recipe/>
-  );
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+
+  //return image source given its rank and suit in string(ex. getCardSrc("3", "hearts"))
+  getCardSrc(rank, suit) {
+    return "./cards/" + rank + "_of_" + suit + ".png" 
+  }
+
+  render()  {
+    return (
+      <div>
+        <h1>HAHA</h1>
+        <img src={this.getCardSrc("2", "hearts")} width="100" height="150"/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Components/recipe.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-
-function Recipe(){
-    return(
-        <h1> Recipe Component</h1>
-    )
-}
-
-export default Recipe
