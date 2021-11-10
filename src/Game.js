653a830cd5d2a69a12aa405c831b195f11e0e847
hyi96 <hyi@ucsb.edu>
2021-11-10 02:13:00 -0800

    running version

--- src/Game.js
+++ src/Game.js
@@ -9,9 +9,11 @@ function orderedDeck() {
     const deck=[]
     const ranks=["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
     const suits=["clubs","hearts","diamonds","spades"]
+    const bitwiseRanks = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+    const bitwiseSuits = [1,4,2,8] 
     for (const suit of suits) {
         for (const rank of ranks) {
-            deck.push({image: <Card rank={rank} suit={suit}/>, rank: ranks.indexOf(rank), suit: suit})
+            deck.push({image: <Card rank={rank} suit={suit}/>, rank: bitwiseRanks[ranks.indexOf(rank)], suit: bitwiseSuits[suits.indexOf(suit)]})
         }
     }
     return deck
@@ -29,12 +31,62 @@ function shuffledDeck() {
     }
     return shuffledDeck;
 }
-//Some constants
+
+//hand rank calculator(algorithm I found online. Not Readable. know how to use it) 
+function evaluateHand(cs, ss) {  //usage: evaluateHand([A,10,J,K,Q],[C,C,C,C,C]), return the name of the hand
+    var pokerHands = ["4 of a Kind", "Straight Flush","Straight","Flush","High Card","1 Pair","2 Pair","Royal Flush", "3 of a Kind","Full House"];
+    var v,i,o,s = 1 << cs[0] | 1 << cs[1] | 1 << cs[2] | 1 << cs[3] | 1 << cs[4];
+    for (i = -1, v = o = 0; i < 5; i++, o = Math.pow(2, cs[i] * 4)) {v += o * ((v / o & 15) + 1);}
+    v = v % 15 - ((s / (s & -s) == 31) || (s == 0x403c) ? 3 : 1);
+    v -= (ss[0] == (ss[1] | ss[2] | ss[3] | ss[4])) * ((s == 0x7c00) ? -5 : 1);
+    return pokerHands[v];
+}
+
+const strengthValues = {"High Card":0, "1 Pair":1, "2 Pair":2, "3 of a Kind":3, "Straight":4, "Flush":5, "Full House":6, "4 of a Kind":7, "Straight Flush":8, "Royal Flush":9}
+
+//return an array containing a player's highest hand strength in each betting round
+function getStrengthArray(playerCards, tableCards) {
+    const strengthArray = [] 
+
+    const playerRanks = [playerCards[0]["rank"], playerCards[1]["rank"]]
+    const playerSuits = [playerCards[0]["suit"], playerCards[1]["suit"]]
+    const tableRanks = [tableCards[0]["rank"], tableCards[1]["rank"], tableCards[2]["rank"], tableCards[3]["rank"], tableCards[4]["rank"]]
+    const tableSuits = [tableCards[0]["suit"], tableCards[1]["suit"], tableCards[2]["suit"], tableCards[3]["suit"], tableCards[4]["suit"]]
+    
+    strengthArray.push(playerRanks[0]===playerRanks[1] ? "1 Pair" : "High Card") //pre-flop: only check if player has pair
+    let highestRank = evaluateHand(playerRanks.concat(tableRanks.slice(0,3)), playerSuits.concat(tableSuits.slice(0,3)))
+    strengthArray.push(highestRank) //flop: one possible combination of player cards and table cards
+    const threeOutFour = [[0,1,3],[0,2,3],[1,2,3]] //possible combinations of 3 elements out of 4 elements, excluding [0, 1, 2]
+    const threeOutFive = [[0,1,4],[0,2,4],[0,3,4],[1,2,4],[1,3,4],[2,3,4]] //possible combinations of 3 elements out of 5 elements, excluding previous 4 combinations
+    for (const combination of threeOutFour) {  
+        const rank = evaluateHand(playerRanks.concat([tableRanks[combination[0]],tableRanks[combination[1]],tableRanks[combination[2]]]), playerSuits.concat([tableSuits[combination[0]],tableSuits[combination[1]],tableSuits[combination[2]]]))
+        if (strengthValues[rank]>strengthValues[highestRank]) highestRank = rank
+    }
+    strengthArray.push(highestRank) //Turn
+    for (const combination of threeOutFive) {
+        const rank = evaluateHand(playerRanks.concat([tableRanks[combination[0]],tableRanks[combination[1]],tableRanks[combination[2]]]), playerSuits.concat([tableSuits[combination[0]],tableSuits[combination[1]],tableSuits[combination[2]]]))
+        if (strengthValues[rank]>strengthValues[highestRank]) highestRank = rank       
+    }
+    strengthArray.push(highestRank) //River
+    strengthArray.push(highestRank) //Showdown
+
+    return strengthArray
+}
+
+//return an integer to represent the strength for ease of comparison between hands 
+function getStrengthValue(strength, cards, tableCards) {
+    const base = 10*(cards[0]["rank"]+cards[1]["rank"]+tableCards[0]["rank"]+tableCards[1]["rank"]+tableCards[2]["rank"]+tableCards[3]["rank"]+tableCards[4]["rank"])
+    return base+10**strengthValues[strength]
+}
+
+//Some game constants
 const DECK = orderedDeck()
 const roundName = ["PreFlop", "Flop", "Turn", "River", "Showdown"]
-const smallestBet = 50 //the smallest amount of chips small blind has to bet
+const smallestBet = 60 //the smallest amount of chips small blind has to bet
 const NUM_PLAYERS = 5
 const positionNames = ["Button", "SB", "BB"]
+
+const initialChips = 1200
 
 //Game Component
 class Game extends React.Component {
@@ -51,6 +103,8 @@ class Game extends React.Component {
             
             playersData: [], // a list of obj = {id:, position:, chips:, bet:, message:, cards:, turn:, action:, folded:}
             tableData: null, //{cards: , pot: , numRevealed: } (numRevealed: number of cards revealed)
+            winnerId: null,
+            message: null,
         }
         //activate class functions that use setState
         this.playerResponded = this.playerResponded.bind(this)
@@ -59,17 +113,43 @@ class Game extends React.Component {
         this.initializeGame()
     }
 
-    getTwoCards(id, deck) {return [deck[id]["image"], deck[51-id]["image"]]} //get two cards from the top and bottom of the ramdomized deck.
+    getTwoCards(id, deck) {return [deck[id], deck[51-id]]} //get two cards from the top and bottom of the ramdomized deck.
+
+    //return a list of player/players who can win the pot
+    findWinner(playing, data) {
+        const nowPlaying = playing
+        const playersData = data
+        //find highest strength value
+        let highestValue = playersData[nowPlaying[0]]["strengthValue"]
+        for (const id of nowPlaying) {
+            if (playersData[id]["strengthValue"] > highestValue) highestValue = playersData[id]["strengthValue"] 
+        }
+        //find which players have the highest value
+        const winnerIDs = []
+        for (const id of nowPlaying) {
+            if (playersData[id]["strengthValue"] === highestValue) winnerIDs.push(id)
+        }
+        return winnerIDs
+    }
 
     initializeGame() {
+        //initialize table
+        const tableCardImages = []
+        const tableCards = []
+        for (let i=0; i<5; i++) {
+            const card = this.state.deck[52/2+i*Math.pow(-1,i)]
+            tableCardImages.push(card["image"])
+            tableCards.push(card)
+        }     
+        this.state.tableData = {cards: tableCardImages, pot: smallestBet*3, numRevealed: 0}
+
         //initialize players
         const playersData = []
         const numPlayers = this.state.numPlayers
-
         for (let i=0; i<numPlayers; i++) {   
             let pos = null
             let initialBet = 0
-            let chips = 1000
+            let chips = initialChips
             const button = this.state.button
             if (button===i) pos=positionNames[0]
             if (button+1===i || button+1-numPlayers===i) {
@@ -82,24 +162,21 @@ class Game extends React.Component {
                 initialBet=smallestBet*2   
                 chips -= initialBet     
             }
+            const cards = this.getTwoCards(i, this.state.deck)
+            const cardImages = [cards[0]["image"], cards[1]["image"]]
+            const strengths = getStrengthArray(cards, tableCards)
+
             playersData.push({id: i, position: pos, chips: chips, bet: initialBet, message: null, 
-                cards: this.getTwoCards(i, this.state.deck), turn: false, action: this.playerResponded, folded: false,
+                cards: cardImages, turn: false, action: this.playerResponded, folded: false, strengths: strengths, 
+                strengthValue: getStrengthValue(strengths[4], cards, tableCards), raised: 0
             })
         }
-
         //populate nowPlaying array starting from currPlayer
         for (let i=this.state.currPlayer; i<playersData.length; i++) {this.state.nowPlaying.push(i)}
         for (let i=0; i<this.state.currPlayer; i++) {this.state.nowPlaying.push(i)}
 
         playersData[this.state.currPlayer]["turn"] = true
         this.state.playersData = playersData
-
-        //initialize table
-        const tableCards = []
-        for (let i=0; i<5; i++) {
-            tableCards.push(this.state.deck[52/2+i*Math.pow(-1,i)]["image"])
-        }     
-        this.state.tableData = {cards: tableCards, pot: smallestBet*3, numRevealed: 0}
     } 
 
     
@@ -111,6 +188,19 @@ class Game extends React.Component {
         const tableData = {...this.state.tableData} //need new cards, reset numRevealed
         let button = null
         playersData[this.state.button]["position"] = null //null current button
+
+        //update table data
+        tableData["numRevealed"] = 0
+        const tableCardImages = []
+        const tableCards = []
+        for (let i=0; i<5; i++) {
+            const card = this.state.deck[52/2+i*Math.pow(-1,i)]
+            tableCardImages.push(card["image"])
+            tableCards.push(card)
+        }     
+        tableData["cards"] = tableCardImages
+        tableData["pot"] = smallestBet*3
+
         //Find the next button
         for (let i=this.state.button+1; i<playersData.length; i++) {
             if (playersData[i]["chips"] !== 0) {
@@ -161,12 +251,17 @@ class Game extends React.Component {
         
         //update remaining players' other properties
         for (const id of nowPlaying) {
-            
-            playersData[id]["cards"] = this.getTwoCards(id, deck)
+            const cards = this.getTwoCards(id, deck)
+            const cardImages = [cards[0]["image"], cards[1]["image"]]
+            playersData[id]["cards"] = cardImages
             playersData[id]["message"] = null
             playersData[id]["turn"] = id === nowPlaying[0] ? true : false
             playersData[id]["folded"] = false
             playersData[id]["bet"] = 0
+            const strengths = getStrengthArray(cards, tableCards)
+            playersData[id]["strengths"] = strengths
+            playersData[id]["strengthValue"] = getStrengthValue(strengths, cards, tableCards)
+            playersData[id]["raised"] = 0
             if (playersData[id]["position"]==="SB") {
                 playersData[id]["bet"] = smallestBet
                 playersData[id]["chips"] -= smallestBet
@@ -176,49 +271,29 @@ class Game extends React.Component {
             }
         }
 
-        //update table data
-        tableData["numRevealed"] = 0
-        const tableCards = []
-        for (let i=0; i<5; i++) {
-            tableCards.push(deck[52/2+i*Math.pow(-1,i)]["image"])
-        }
-        tableData["cards"] = tableCards
-        tableData["pot"] = smallestBet*3
-
         this.setState({
-            numPlayers: NUM_PLAYERS,
-            round: 0,
-            playersData: playersData,
-            nowPlaying: nowPlaying,
-            tableData: tableData,
-            highestBet: 0,
-            deck: deck,
-            currPlayer: nowPlaying[firstToAct],
+            numPlayers: NUM_PLAYERS, round: 0, playersData: playersData, nowPlaying: nowPlaying, 
+            tableData: tableData, highestBet: 0, deck: deck, currPlayer: nowPlaying[firstToAct],
         })
     }
 
     playerResponded(id, choice) {
         const playersData = [...this.state.playersData] //assigned by value
         const player = playersData[id] //assigned by reference 
+        let highestBet = this.state.highestBet
 
         if (choice === "Increase") { //increase bet
-            if (player["bet"] < player["chips"]) {
-                player["bet"] += 50
-                if (player["bet"] === player["chips"]) player["message"] = "All in"
+            if (player["bet"]+player["raised"] < player["chips"]) {
+                player["raised"] += smallestBet
+                if (player["bet"]+player["raised"] === player["chips"]) player["message"] = "All in"
             }
-            let highest = this.state.highestBet
-            if (player["bet"]>highest) highest = player["bet"] 
-            else player["bet"] = highest
-            this.setState({
-                playersData: playersData,
-                highestBet: highest,
-            })
-        }
-        
-        else { 
+            if (player["bet"]+player["raised"]<highestBet) player["raised"] = highestBet - player["bet"] + smallestBet
+            this.setState({playersData: playersData})
+        } else { 
             const tableData = {...this.state.tableData} //assigned by value
             const nowPlaying = [...this.state.nowPlaying]
             let round = this.state.round
+            
 
             //search next player's id
             const playingIndex = nowPlaying.indexOf(id)
@@ -226,8 +301,7 @@ class Game extends React.Component {
             if (playingIndex === nowPlaying.length-1) {
                 nextId = nowPlaying[0]
                 round += 1
-            }
-            else nextId = nowPlaying[playingIndex+1]
+            } else nextId = nowPlaying[playingIndex+1]
 
             //these are actions that end a player's turn
             if (choice === "Fold") {
@@ -243,16 +317,15 @@ class Game extends React.Component {
                 
                 if (nowPlaying.length === 1) { //end this game round early if all except one player folded
                     playersData[nowPlaying[0]]["chips"] += tableData["pot"] 
-
+                    const message = "Player" + nowPlaying[0].toString() + " won the pot of " + tableData["pot"].toString() + " chips!!!"
                     tableData["pot"] = 0
-                    this.setState({playersData: playersData, tableData: tableData, currPlayer: nextId, nowPlaying: nowPlaying, round: round})
-                    this.newRound()
+                    this.setState({playersData: playersData, tableData: tableData, currPlayer: nextId, nowPlaying: nowPlaying, round: 4, message: message})
                     return
                 }
             } else { 
                 if (choice === "Check/Call") {
                     //set current player's bet to match highest bet
-                    let difference = this.state.highestBet - player["bet"] 
+                    let difference = highestBet - player["bet"] 
                     if (difference + player["bet"] >= player["chips"]) { //if proposed bet amount is greater than the amount of the chips the player has
                         difference = player["chips"] - player["bet"]
                         player["message"] = "All in"
@@ -261,68 +334,72 @@ class Game extends React.Component {
                     player["bet"] += difference
                     tableData["pot"] += difference 
                     player["chips"] -= difference
-                } else {
-                    tableData["pot"] += player["bet"] 
-                    player["chips"] -= player["bet"]
+                } else {//Raise
+                    player["chips"] -= player["raised"]
+                    tableData["pot"] += player["raised"]
+                    player["bet"] += player["raised"]  
+                    highestBet = player["bet"]   
                 }
+                player.raised = 0   
             }
-            
             //end current player's turn and start next player's turn
             player["turn"] = false
             playersData[nextId]["turn"] = true
 
-            function resetBets() { 
-                for (const index of nowPlaying) {
-                    let bet = 0
-                    if (playersData[index]["position"] === "SB") bet = smallestBet
-                    else if (playersData[index]["position"] === "BB") bet = smallestBet*2
-                    playersData[index]["bet"] = bet
-                    playersData[index]["chips"] -= bet
-                    tableData["pot"] += bet
-                }
-            }
-    
+
             //check if the next round should begin
+            let message = ""
             if (round !== this.state.round) {
-                resetBets()
-                if (round === 1) { //Flop
-                    tableData["numRevealed"] = 3
-                }
-                else if (round === 2) { //Turn
-                    tableData["numRevealed"] = 4       
-                }
-                else if (round === 3) { //River
-                    tableData["numRevealed"] = 5        
-                }
-                else { //calculate winner 
-
+                if (round === 1) tableData["numRevealed"] = 3 //Flop
+                else if (round === 2) tableData["numRevealed"] = 4 //Turn    
+                else if (round === 3) tableData["numRevealed"] = 5 //River
+                else { //Showdown, find winner, distribute pot
+                    const winnerIDs = this.findWinner(nowPlaying, playersData) 
+                    if (winnerIDs.length === 1) {
+                        message = "Player" + winnerIDs[0].toString() + " won the pot of " + tableData["pot"].toString() + " chips!!!"
+                        playersData[winnerIDs[0]]["chips"] += tableData["pot"]
+                    } else { //split the pot if more than one player win
+                        const share = tableData["pot"] / winnerIDs.length
+                        for (const id of winnerIDs) {
+                            playersData[id]["chips"] += share
+                            message += "Player" + id.toString() + ", "
+                        }
+                        message += "share the pot of " + tableData["pot"].toString() + " chips!!!"
+                    } 
+                    tableData["pot"] = 0
                 }
             }
 
-            this.setState({playersData: playersData, tableData: tableData, currPlayer: nextId, nowPlaying: nowPlaying, round: round})
+            this.setState({playersData: playersData, tableData: tableData, currPlayer: nextId, nowPlaying: nowPlaying, round: round, message: message, highestBet: highestBet})
         }
     }
 
-
     render()  {
-        //use playerData to map an array of Player Components
+        //use playersData to map an array of Player Components
         const playerComponents = this.state.playersData.map(player => <Player id={player.id} 
             position={player.position}
             chips={player.chips}
             bet={player.bet}
+            highestBet={this.state.highestBet}
             message={player.message}
             cards={player.cards}
             turn={player.turn}
             action={player.action}
             folded={player.folded}
+            strengths={player.strengths}
+            round={this.state.round}
+            raised={player.raised}
         />)
         const table = this.state.tableData
         const tableComponent = <Table cards={table["cards"]} pot={table["pot"]} numRevealed={table.numRevealed}/>
+        const newDeckButton = <div><h1>{this.state.message}</h1><button onClick={this.newRound}>New Deck</button></div>
+        const gameStatus = <h4>Current Player:{this.state.currPlayer} Betting Round:{roundName[this.state.round]}</h4>
+
         return (
             <div>
-                {tableComponent}
+                {this.state.round===4 ? newDeckButton : gameStatus}
                 <hr/>
-                <h4>Current Player:{this.state.currPlayer} Round:{roundName[this.state.round]}</h4>
+                {tableComponent}
                 <hr/>
                 {playerComponents}
                 {tableComponent}
--- src/Player.js
+++ src/Player.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 //Player Component
-//props={id, position, chips, bet, message, cards, turn, action, folded}
+//props={id, position, chips, bet, message, cards, turn, action, folded, strengths, round, highestBet}
 function Player(props) {
     if (!props.folded) {
         return (
             <div>
-                <h3>Player{props.id} {props.position} Chips:{props.chips} Bet:{props.bet} {props.message}</h3> 
+                <h3>Player{props.id} [{props.position}] [Chips:{props.chips}] [Bet:{props.bet}] {props.message} {(props.raised>0) && <div>Raise bet to:{props.raised+props.bet}</div>}</h3> 
                 <div>
-                    {props.cards}
+                    {props.cards} {props.strengths[props.round]}
                 </div>
                 {
                     props.turn &&
                     <div>
                         <button onClick={() => props.action(props.id, "Check/Call")}>Check/Call</button>
                         <button onClick={() => props.action(props.id, "Fold")}>Fold</button>
-                        <button onClick={() => props.action(props.id, "Raise")}>Raise</button>
+                        {(props.bet+props.raised > props.highestBet) && <button onClick={() => props.action(props.id, "Raise")}>Raise</button>}
                         <button onClick={() => props.action(props.id, "Increase")}>Increase bet amount</button>
                     </div>
                 }
